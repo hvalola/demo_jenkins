58a782b496003ccbe635ea3bf50158c8c41c9d03
pubudu_r <pubudu.r@cba.lk>
2022-09-21 10:52:29 +0530

    demo commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("test my data");
         SpringApplication.run(DemoApplication.class, args);
     }
 
