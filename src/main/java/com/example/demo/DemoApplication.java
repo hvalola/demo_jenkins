279654664dc965a7ae9abda1f836076818f2bee0
pubudu_r <pubudu.r@cba.lk>
2022-09-23 13:18:22 +0530

    updated to docker build 4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("test my data");
+        System.out.println("test my data");System.out.println("test my data");
+
         System.out.println("test my data3");
         System.out.println("test my data3");System.out.println("test my data3");
         SpringApplication.run(DemoApplication.class, args);
