bfa1550229443f6b8ca388d5252a7efd608d89b1
pubudu_r <pubudu.r@cba.lk>
2022-09-23 11:26:32 +0530

    updated to mvn build 3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         System.out.println("Hello world");
         System.out.println("test my data");
         System.out.println("test my data3");
+        System.out.println("test my data3");System.out.println("test my data3");
         SpringApplication.run(DemoApplication.class, args);
     }
 
